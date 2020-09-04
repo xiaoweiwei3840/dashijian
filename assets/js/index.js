aafa66b38a2b8861d969d52515083f16cbe48df8
weiwei <wewewe@163.com>
2020-09-04 21:14:36 +0800

    修复

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,19 +31,14 @@ function get() {
 
 
 $('.layui-nav-item').on('click', function (e) {
-    // e.preventDefault();
     console.log(1);
     layer.confirm('确定退出登录?', {
         icon: 3,
         title: '提示'
     }, function (index) {
-        //do something
-        // 1. 清空本地存储中的 token
         localStorage.removeItem('token')
-        // 2. 重新跳转到登录页面
+    
         location.href = '/login.html'
-
-        // 关闭 confirm 询问框
         layer.close(index)
     })
 
